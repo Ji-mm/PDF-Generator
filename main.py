6f4c857b95b0a68bc5d1836f34f766c45597840b
Atudo <jimpirate49@gmail.com>
2023-02-01 14:02:12 +0300

    exercise: add lines

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(150, 150, 150)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 21, 200, 21)
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)
 
     # Set the footer
     pdf.ln(260)
@@ -28,6 +29,8 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="R", ln=1)
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 
 pdf.output("output3.pdf")
